2b5e68cbe1afaf09d3b2da3eec6321a9fbe82dfb
Hangtao Du <du.han@husky.neu.edu>
2018-09-13 20:19:02 -0400

    Update lab_0.cpp

--- lab_0.cpp
+++ lab_0.cpp
@@ -1,1 +1,43 @@
-# testEECE
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+int i;
+bool finished = true;
+
+while(finished) {
+cout << "Main menu: \n\n1. Print the array\n2. Add one element\n3. Remove last element\n4. Exit\n\nSelect an option: ";
+
+cin >> i;
+
+if (cin.fail())
+   {
+cin.clear();
+cin.ignore(); 
+      cout << "You did not input a number, Please enter again.\n" << endl;
+   }
+else {
+if(i < 1 || i > 4){
+cin.clear();
+cin.ignore();
+      cout << "The option does not exist, pleas enter again.\n" << endl;
+}
+else if (i == 1){
+cout << "You selected\"Print the array\"\n" << endl;
+}
+else if (i == 2){
+cout << "You selected\"Add one element\"\n" << endl;
+}
+else if (i == 3){
+cout << "You selected\"Remove last elementt\"\n" << endl;
+}
+else {
+cout << "You selected\"Exit\"\n " << endl;
+finished = false;
+}
+}
+}
+return 0;
+}
